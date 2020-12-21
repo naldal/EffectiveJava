b9cc5762cdd71f770815cdbcd487382c462981f5
naldal <cec7378@gmail.com>
2020-12-21 21:20:00 +0900

    22p

--- src/BuilderPattern/NyPizza.java
+++ src/BuilderPattern/NyPizza.java
@@ -6,7 +6,17 @@ public class NyPizza extends Pizza {
     public enum Size { SMALL, MEDIUM, LARGE }
     private final Size size;
 
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+
+    @Override public String toString() {
+        return toppings + "로 토핑한 뉴욕 피자";
+    }
+
     public static class Builder extends Pizza.Builder<Builder> {
+
         private final Size size;
 
         public Builder(Size size) {
@@ -16,16 +26,7 @@ public class NyPizza extends Pizza {
         @Override public NyPizza build() {
             return new NyPizza(this);
         }
-
         @Override protected Builder self() { return this; }
-    }
 
-    private NyPizza(Builder builder) {
-        super(builder);
-        size = builder.size;
-    }
-
-    @Override public String toString() {
-        return toppings + "로 토핑한 뉴욕 피자";
     }
 }
--- src/BuilderPattern/Pizzatest.java
+++ src/BuilderPattern/Pizzatest.java
@@ -8,6 +8,7 @@ public class Pizzatest {
     public static void main(String[] args) {
         NyPizza pizza = new NyPizza.Builder(SMALL)
                 .addTopping(SAUSAGE).addTopping(ONION).build();
+
         Calzone calzone = new Calzone.Builder()
                 .addTopping(HAM).sauceInside().build();
 
