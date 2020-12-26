fa5ce5efa02ae64dc5b70ae642c0d1bf38b09bfa
naldal <cec7378@gmail.com>
2020-12-26 22:48:28 +0900

    48 p

---
+++ src/chapter2/Item9/TopLineWithDefault.java
@@ -0,0 +1,22 @@
+package chapter2.Item9;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TopLineWithDefault {
+
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        System.out.println(firstLineOfFile(path));
+    }
+}
