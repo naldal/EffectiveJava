405147787b3b25d60e24e3e60493e1c241061841
naldal <cec7378@gmail.com>
2021-01-14 22:53:56 +0900

    56p

--- src/chapter3/Item10/Equals.java
+++ src/chapter3/Item10/Equals.java
@@ -3,6 +3,14 @@ package chapter3.Item10;
 import java.util.Objects;
 
 public class Equals {
+
+    public static void main(String[] args) {
+        CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
+        String s = "polish";
+
+        System.out.println(cis.equals(s));
+        System.out.println(s.equals(cis));
+    }
     
     /** 
      * equals 메서드를 재정의할 때는 반드시 일반 규약을 따라야 한다. 다음은 Object 명세에 적힌 규약이다
@@ -24,6 +32,7 @@ final class CaseInsensitiveString {
     }
     
     // 대칭성 위배!
+/*
     @Override public boolean equals(Object o) {
         if (o instanceof CaseInsensitiveString)
             return s.equalsIgnoreCase(
@@ -33,6 +42,7 @@ final class CaseInsensitiveString {
         return false;
         
     }
+*/
     // ... 나머지 코드 생략
     /**
      * 예를 들어, 다음처럼 CaseInsensitiveString 과 일반 String 객체가 하나씩 있다고 해보자.
@@ -42,4 +52,15 @@ final class CaseInsensitiveString {
      * cis.equals(s)는 true를 반환하지만, String의 equals는 CaseInsensitive 의 기능이 존재하지 않는다.
      * 따라서 s.equals(cis)는 false를 반환하게 되고 이것은 대칭성을 명백히 위반한다
      */
+
+    /**
+     * 이 문제를 해결하려면 CaseInsensitiveString 의 equals를 String과도 연동하겠다는 허황된 꿈을 버려야한다.
+     * 그 결과 equals는 다음과 같은 모습으로 바뀐다.
+     */
+
+    @Override public boolean equals(Object o) {
+        return o instanceof CaseInsensitiveString &&
+                ((CaseInsensitiveString)o).s.equalsIgnoreCase(s);
+    }
+
 }
